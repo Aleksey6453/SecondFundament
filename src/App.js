8ca5a79e1d024040a571f08edf2dffc5f1fa9cef
Aleksey6453 <login6453@gmail.com>
2023-08-16 01:54:17 +0300

    add searhc by title and add registr

--- src/App.js
+++ src/App.js
@@ -6,22 +6,36 @@ import {Test} from './components/test/Test'
 import {Form} from './components/form/Form'
 import { PostList } from './components/postList/PostList'
 import {MySelect} from './components/UI/select/MySelect'
+import Search from './components/search/Search';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
-  // const titleInputRef = React.useRef()
-  const [selectedSort, setSelectedSort] = React.useState('')
-
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    console.log(sort)
-    setPosts([...posts].sort((a,b)=>a[sort].localeCompare(b[sort])))
-  }
-
+  
   const [posts, setPosts] = React.useState([
     {id: 1, title: 'TT ', body: 'body text'},
     {id: 2, title: 'ZZ ', body: 'body text'},
     {id: 3, title: 'AA ', body: 'body text'}
   ])
+
+  const [searchQuery, setSearchQuery] = React.useState('')
+  const [selectedSort, setSelectedSort] = React.useState('')
+
+  const sortedPosts = React.useMemo(()=>{
+    console.log('func happened SortedPosts')
+    if(selectedSort){
+      return [...posts].sort((a,b)=>a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPost = React.useMemo(()=>{
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(sort)
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -37,20 +51,24 @@ function App() {
           setPosts={setPosts}
           posts={posts}
           create={createPost}
-          // titleInputRef={titleInputRef}
           />
-      <MySelect 
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue='sort by...'
-        options={[
-          {value:'title', name:'by title'},
-          {value:'body', name:'by body'}
-        ]}
-      />
+      <div className="gorizont">  
+        <MySelect 
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='sort by...'
+          options={[
+            {value:'title', name:'by title'},
+            {value:'body', name:'by body'}
+          ]}
+        />
+        
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>  
+      </div>    
+      
       {
         posts.length 
-          ?  <PostList remove={removePost} posts={posts} Post={Post} /> 
+          ?  <PostList remove={removePost} posts={sortedAndSearchedPost} Post={Post} /> 
           : <h1>There is empty!</h1>
       }      
       
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Search.module.css'
+import MyInput from '../UI/input/MyInput'
+
+const Search = ({searchQuery, setSearchQuery}) => {
+  return (
+    <div className={styles.search}>
+      <MyInput 
+            placeholder='Search...'
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default Search
