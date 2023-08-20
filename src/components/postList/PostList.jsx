90ecd4ef7de34ea0a852908d4213655255d76687
Aleksey6453 <login6453@gmail.com>
2023-08-20 17:49:52 +0300

    add animation for postlist from react transition group

--- src/components/postList/PostList.jsx
+++ src/components/postList/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './PostList.module.css'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 const PostList = ({posts, Post, remove}) => {
   if(!posts.length){
@@ -9,7 +10,19 @@ const PostList = ({posts, Post, remove}) => {
   return (
     <div className={styles.postList}>
        <h1>List of Posts</h1>
-       {
+       <TransitionGroup>
+        {posts.map((post, index) => 
+            <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+            >
+                <Post remove={remove} number={index + 1} post={post}  />
+            </CSSTransition>
+        )}
+      </TransitionGroup>
+      
+       {/* {
         posts.map((post, index) => 
           <Post post={post} 
                 key={post.id} 
@@ -20,7 +33,7 @@ const PostList = ({posts, Post, remove}) => {
                 number={index + 1} 
                 />
         )
-      }
+      } */}
     </div>
   )
 }
