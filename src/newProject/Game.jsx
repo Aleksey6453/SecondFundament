985c7f889c7f4f79e269389bbaa73bf60523564e
Aleksey6453 <login6453@gmail.com>
2023-10-15 15:33:24 +0300

    add new version game

--- src/newProject/Game.jsx
+++ src/newProject/Game.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import styles from './NewProject.module.css'
 
+const symbolX = 'X'
+const symbolO = 'O'
+
 const Game = () => {
+    const cells = [symbolX, null, null, symbolO, null, null, symbolO, symbolX, null]
+    const currentStep = symbolX;
+    const getSymbolClassName = (symbol) => {
+        if(symbol === symbolX) return 'symbolX'
+        if(symbol === symbolO) return 'symbolO'
+        return ''
+    }
+    const renderSymbol = (symbol) => <span className={`symbol ${getSymbolClassName(symbol)}`}> {symbol} </span>
+
   return (
     <div className='game'>
         <div className='gameInfo'>
-            <h1>Game</h1>
+            <h1>Step '{renderSymbol(currentStep)}' </h1>
         </div>
         <div className={styles.gameField}>
-            <button className={styles.btn}>1</button>
-            <button className={styles.btn}>2</button>
-            <button className={styles.btn}>3</button>
-            <button className={styles.btn}>4</button>
-            <button className={styles.btn}>5</button>
-            <button className={styles.btn}>6</button>
-            <button className={styles.btn}>7</button>
-            <button className={styles.btn}>8</button>
-            <button className={styles.btn}>9</button>
+            {cells.map((symbol, index) => {
+                return(
+                    <button key={index} className='cell cellWin'>
+                        {symbol ? renderSymbol(symbol) : null} 
+                    </button>
+                )
+            })}
+            {/* <button className='cell cellWin'><span>x</span></button>
+            <button className='cell '>0</button>
+            <button className='cell '></button>
+            <button className='cell cellWin'>x</button>
+            <button className='cell '></button>
+            <button className='cell '>0</button>
+            <button className='cell cellWin'>x</button>
+            <button className='cell '></button>
+            <button className='cell '></button> */}
+          
 
         </div>
       
