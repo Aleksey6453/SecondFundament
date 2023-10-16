1d979f229c96e8c373a888825cc4ff9aa9afe1e3
Aleksey6453 <login6453@gmail.com>
2023-10-17 00:52:23 +0300

    add new code

--- src/newProject/Game.jsx
+++ src/newProject/Game.jsx
@@ -4,10 +4,34 @@ import styles from './NewProject.module.css'
 const symbolX = 'X'
 const symbolO = 'O'
 
+const computeWinner = (cells) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++){
+        const[a, b, c] = lines[i];
+        if (
+            cells[a] &&
+            cells[a] === cells[b] &&
+            cells[a] === cells[c]
+        ) {
+            return [a, b, c]
+        }
+    }
+}
+
 const Game = () => {
    
     const [cells, setCells] = React.useState([null, null, null, null, null, null, null, null, null])
     const [currentStep, setCurrentStep] = React.useState(symbolX)
+    const [winnerCombi, setWinnerCombi] = React.useState()
     // const currentStep = symbolX;
     const getSymbolClassName = (symbol) => {
         if(symbol === symbolX) return 'symbolX'
@@ -22,8 +46,11 @@ const Game = () => {
         }
         const cellsCopy = cells.slice()
         cellsCopy[index] = currentStep;
+        const winner = computeWinner(cellsCopy);
+        
         setCells(cellsCopy)
         setCurrentStep(currentStep === symbolX ? symbolO : symbolX)
+        setWinnerCombi(winner)
     }
     
 
@@ -35,8 +62,11 @@ const Game = () => {
         <div className={styles.gameField}>
             {cells.map((symbol, index) => {
                 return(
-                    <button key={index} onClick={()=>handleCellClick(index)} className='cell cellWin'>
-                        {symbol ? renderSymbol(symbol) : null} 
+                    <button key={index} 
+                            onClick={()=>handleCellClick(index)} 
+                            className='cell cellWin'
+                            >
+                                {symbol ? renderSymbol(symbol) : null} 
                     </button>
                 )
             })}
